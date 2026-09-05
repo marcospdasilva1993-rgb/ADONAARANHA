668b059fbe3aaf4d56e49f897e2d81742ffb268a
marco <marcospdasilva1993@gmail.com>
2026-09-04 22:21:26 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Dona Aranha - Galinha Pintadinha");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("A dona aranha subiu pela parede\nVeio a chuva forte e a derrubou\nJá passou a chuva o Sol já vai surgindo\nE a dona aranha continua a subir");
+Console.WriteLine("");
+Console.ResetColor();
+Console.WriteLine("Ela é teimosa e desobediente\nSobe, sobe, sobe e nunca está contente\nA dona aranha subiu pela parede\nVeio a chuva forte e a derrubou");
+Console.WriteLine("");
+
+Console.WriteLine("Já passou a chuva o Sol já vai surgindo\nE a dona aranha continua a descer\nEla é teimosa e desobediente\nDesce, desce, desce e nunca está contente");
+Console.WriteLine("");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("A dona aranha subiu pela parede\nVeio a chuva forte e a derrubou|\nJá passou a chuva o Sol já vai surgindo\nE a dona aranha continua a subir");
+Console.WriteLine("");
+Console.ResetColor();
+
+Console.WriteLine("Ela é teimosa e desobediente\nSobe, sobe, sobe e nunca está contente");
